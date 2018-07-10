5878703501694b56decb66d356d425a68fa4eeba
Ikeda <ruizuikeda@gmail.com>
2018-07-10 08:25:47 -0300

    aula 11

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,14 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-    url = 'http:192.168.15.59';
-    cursoAngular = true;
-    urlImg = 'http://placeholder.pics/svg/400x200';
+    url             = 'http:192.168.15.59';
+    cursoAngular    = true;
+    urlImg          = 'http://placeholder.pics/svg/400x200';
+    valorAtual      = '';
+    valorSalvo      = '';
+    isMouseOver     = false;
 
     getValor() {
         return 1;
     }
-
+    botaoClicado() {
+        alert('clicou');
+    }
+    onKeyUp(evento: KeyboardEvent) {
+        this.valorAtual = (<HTMLInputElement>evento.target).value;
+    }
+    salvarValor(valor) {
+        this.valorSalvo = valor;
+    }
+    // mouse
+    onMouseOver() {
+        this.isMouseOver = true;
+    }
+    onMouseOut() {
+        this.isMouseOver = !this.isMouseOver;
+    }
     constructor() { }
 
     ngOnInit() {
