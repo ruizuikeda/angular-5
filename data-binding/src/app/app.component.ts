c61708c1330df38995528451cf0f0b619e28317d
Ikeda <ruizuikeda@gmail.com>
2018-07-10 10:22:11 -0300

    fix alert

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'app';
     valor = 25;
-    deletarCiclo: false;
+    deletarCiclo = false;
 
     mudarValor() {
         this.valor++;
