d0cf7d142a6c2d1f86a64cbff960ca3a33be82c0
Ikeda <ruizuikeda@gmail.com>
2018-07-10 10:14:02 -0300

    aula 15 - Ciclo de vida do Componente

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+    valor = 25;
+    deletarCiclo: false;
+
+    mudarValor() {
+        this.valor++;
+    }
+    destruirCiclo() {
+        this.deletarCiclo = true;
+    }
 }
